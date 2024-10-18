54d06004318636e79505b138db510cdb84d70b67
flm <ooohmydawn@hotmail.com>
2024-10-18 13:28:54 +0800

    feat: fileName edit

--- src/components/CodeEditor/FileNameList.tsx
+++ src/components/CodeEditor/FileNameList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { PlayGroundContext } from "../playground/Context";
 import { cn } from "../../libs/classnames";
 import "./index.modules.css";
@@ -6,10 +6,27 @@ interface FileNameItemProps {
   value: string;
   checked: boolean;
   onClick: () => void;
+  onEditComplete: (name: string) => void;
 }
 
 const FileNameItem: React.FC<FileNameItemProps> = (props) => {
-  const { value, checked, onClick } = props;
+  const { value, checked, onClick, onEditComplete } = props;
+
+  const [name, setName] = useState(value);
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleDoubleClick = () => {
+    setEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const handleBlur = () => {
+    setEditing(false);
+    onEditComplete(name);
+  };
 
   return (
     <div
@@ -19,13 +36,23 @@ const FileNameItem: React.FC<FileNameItemProps> = (props) => {
       )}
       onClick={onClick}
     >
-      <span>{value}</span>
+      {editing ? (
+        <input
+          ref={inputRef}
+          value={name}
+          className="w-[90px] py-1 pl-0 pr-2.5 text-sm text-neutral-800 bg-neutral-200 border rounded-[4px] outline-none"
+          onChange={(e) => setName(e.target.value)}
+          onBlur={handleBlur}
+        />
+      ) : (
+        <span onDoubleClick={handleDoubleClick}>{name}</span>
+      )}
     </div>
   );
 };
 
 const FileNameList = () => {
-  const { files, selectedFileName, setSelectedFileName } =
+  const { files, selectedFileName, setSelectedFileName, updateFileName } =
     useContext(PlayGroundContext);
 
   const [tabs, setTabs] = useState([""]);
@@ -33,6 +60,11 @@ const FileNameList = () => {
   useEffect(() => {
     setTabs(Object.keys(files));
   }, [files]);
+
+  const handleEditComplete = (name: string, preName: string) => {
+    updateFileName(preName, name);
+    setSelectedFileName(name);
+  };
 
   return (
     <div className="tabs flex items-center h-9 overflow-x-auto overflow-y-hidden border-b box-border text-neutral-900 bg-white">
@@ -42,6 +74,7 @@ const FileNameList = () => {
           value={item}
           checked={selectedFileName === item}
           onClick={() => setSelectedFileName(item)}
+          onEditComplete={(name) => handleEditComplete(name, item)}
         />
       ))}
     </div>
