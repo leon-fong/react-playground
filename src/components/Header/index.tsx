a3090b471b5752b19dee1ed223141a4fbe1c370b
flm <ooohmydawn@hotmail.com>
2024-10-18 15:12:42 +0800

    feat: add share button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import Logo from '../../assets/react.svg';
+import React from "react";
+import Logo from "../../assets/react.svg";
+import { CarbonShare } from "../../assets/icons/ShareIcon";
+import copy from "copy-to-clipboard";
 
 const Header = () => {
   return (
-    <div className='h-[50px] px-5 box-border border-b flex items-center justify-between '>
-    <div className='flex text-xl items-center'>
-    <img src={Logo} alt="logo" className='h-6 mr-2.5' />
-    <span>React Playground</span>
+    <div className="h-[50px] px-5 box-border border-b flex items-center justify-between ">
+      <div className="flex text-xl items-center">
+        <img src={Logo} alt="logo" className="h-6 mr-2.5" />
+        <span>React Playground</span>
+      </div>
+      <div>
+        <CarbonShare
+          onClick={() => {
+            copy(window.location.href);
+            window.alert("Sharable URL has been copied to clipboard.");
+          }}
+        />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
